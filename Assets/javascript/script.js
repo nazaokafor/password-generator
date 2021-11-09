75b26c5018e18758750b405dd377ebe626230d9a
Naza Okafor <91860335+nazaokafor@users.noreply.github.com>
2021-11-09 13:31:27 -0500

    official edit 1

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// WHEN I click the button to generate a password
+
+function clickButton() {
+var createPassword = window.prompt("Create a new password.");
+clickButton(createPassword);
+return;
+}
+
+
+// THEN I am presented with a series of prompts for password criteria
+
+var criteria = ["Password Length", "Character Type"];
+
+var generatePassword = function () {
+  //  = window.prompt("Enter Password Length or Character Type");
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +30,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Look up "prompt", "alert", and "confirm" on google
+
