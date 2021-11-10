e064665f390baea39431a6d5df3c0dfdb91546e7
Naza Okafor <91860335+nazaokafor@users.noreply.github.com>
2021-11-09 19:20:06 -0500

    official edit 2

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,35 +1,38 @@
-// Assignment Code
+// *Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // WHEN I click the button to generate a password
 
-function clickButton() {
-var createPassword = window.prompt("Create a new password.");
-clickButton(createPassword);
-return;
-}
+// function clickButton() {
+// var createPassword = window.prompt("Create a new password."); clickButton(createPassword);
+// return;
+// }
 
 
 // THEN I am presented with a series of prompts for password criteria
 
-var criteria = ["Password Length", "Character Type"];
+// var criteria = ["Password Length", "Character Type"];
 
-var generatePassword = function () {
+// var generatePassword = function () {
   //  = window.prompt("Enter Password Length or Character Type");
-}
+// }
 
 
-// Write password to the #password input
+// *Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// *Add event listener to generate button
+generateBtn.addEventListener("click", writePassword(generateBtn) {
+  
+}
+);
 
 // Look up "prompt", "alert", and "confirm" on google
 
